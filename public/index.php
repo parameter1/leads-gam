e34c3da80f085e973195fc62c888babe98ea141b
Jacob Bare <jacob@limit0.io>
2019-02-04 14:37:08 -0600

    Display the GAM network name and code

--- public/index.php
+++ public/index.php
@@ -3,11 +3,21 @@ require '../vendor/autoload.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Google\AdsApi\AdManager\v201811\NetworkService;
 use Limit0\Test;
+use Limit0\AdManager\ServiceFactory;
 
 $app = new \Slim\App;
 $app->get('/', function(Request $req, Response $res, array $args) {
-  $data = ['ping' => 'pong', 'foo' => Test::foo()];
-  return $res->withJson($data);
+  $factory = new ServiceFactory();
+  $networkService = $factory->service(NetworkService::class);
+  $network = $networkService->getCurrentNetwork();
+
+  $res->getBody()->write(sprintf(
+    '<pre>%s (%s)</pre>',
+    $network->getDisplayName(),
+    $network->getNetworkCode()
+  ));
+  return $res;
 });
 $app->run();
