32ab76e75c908395f41952b2170052edf8fc2d5b
Jacob Bare <jacob@limit0.io>
2019-02-04 09:01:32 -0600

    Add slim test route

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,12 @@
 <?php
-echo 'Hello, world!';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function(Request $req, Response $res, array $args) {
+  $data = ['ping' => 'pong'];
+  return $res->withJson($data);
+});
+$app->run();
