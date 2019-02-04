d6744241c0dc38cb12cf9d60309338fc91540f32
Jacob Bare <jacob@limit0.io>
2019-02-04 09:11:35 -0600

    Move public to root folder

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
+require_once '../vendor/autoload.php';
+
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-
-require '../../vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/', function(Request $req, Response $res, array $args) {
