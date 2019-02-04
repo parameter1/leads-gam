b3ab5919c7e51939a218684b4baca84f17bb785a
Jacob Bare <jacob@limit0.io>
2019-02-04 15:05:29 -0600

    Create HomeController and move index logic

--- public/index.php
+++ public/index.php
@@ -2,22 +2,9 @@
 require '../vendor/autoload.php';
 require 'container.php';
 
-use Google\AdsApi\AdManager\v201811\NetworkService;
+use Limit0\Controller\HomeController;
 use Slim\App;
-use \Psr\Http\Message\ResponseInterface as Response;
-use \Psr\Http\Message\ServerRequestInterface as Request;
 
 $app = new App($container);
-$app->get('/', function(Request $req, Response $res, array $args) {
-  $factory = $this->get('ad-manager.service-factory');
-  $networkService = $factory->service(NetworkService::class);
-  $network = $networkService->getCurrentNetwork();
-
-  $res->getBody()->write(sprintf(
-    '<pre>%s (%s)</pre>',
-    $network->getDisplayName(),
-    $network->getNetworkCode()
-  ));
-  return $res;
-});
+$app->get('/', HomeController::class . ':index');
 $app->run();
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Limit0\Controller;
+
+use Google\AdsApi\AdManager\v201811\NetworkService;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class HomeController
+{
+  /**
+   * @var ContainerInterface
+   */
+  protected $container;
+
+  /**
+   * @param ContainerInterface $container
+   */
+  public function __construct(ContainerInterface $container)
+  {
+    $this->container = $container;
+  }
+
+  /**
+   * @param Request $req
+   * @param Response $res
+   * @param array $args
+   */
+  public function index(Request $req, Response $res, array $args)
+  {
+    $factory = $this->container->get('ad-manager.service-factory');
+    $networkService = $factory->service(NetworkService::class);
+    $network = $networkService->getCurrentNetwork();
+
+    $res->getBody()->write(sprintf(
+      '<pre>%s (%s)</pre>',
+      $network->getDisplayName(),
+      $network->getNetworkCode()
+    ));
+    return $res;
+  }
+}
