2b3c16b102ba63381f6a46d4320ab505b150934a
Josh Worden <josh@parameter1.com>
2020-08-19 14:59:24 -0500

    Change build target to p1

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,6 +3,6 @@ set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker build -t "leads-gam:$TRAVIS_TAG" .
-docker tag "leads-gam:$TRAVIS_TAG" "limit0/leads-gam:$TRAVIS_TAG"
-docker push "limit0/leads-gam:$TRAVIS_TAG"
+docker tag "leads-gam:$TRAVIS_TAG" "parameter1/leads-gam:$TRAVIS_TAG"
+docker push "parameter1/leads-gam:$TRAVIS_TAG"
 docker image rm "leads-gam:$TRAVIS_TAG"
