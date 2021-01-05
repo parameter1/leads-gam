4a52385130d39b161ac3baeaa16b2b3aafac52eb
Josh Worden <josh@parameter1.com>
2021-01-05 15:21:18 -0600

    Update Google API version

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace Limit0\Controller;
 
-use Google\AdsApi\AdManager\v201811\NetworkService;
+use Google\AdsApi\AdManager\v202011\NetworkService;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
